6b18b7ae4ff7847c1383fd66d9502a3952f06004
jadiwal <deepak.jadiwal2@gmail.com>
2025-03-01 09:59:02 +0530

    Comment Console

--- app/pokemon/page.jsx
+++ app/pokemon/page.jsx
@@ -50,7 +50,7 @@ export default function Home() {
 //   const handleEdit = data => {
 //     router.push(`/home/${data.id}`)
 // }
-console.log(filteredPokemons, "filteredPokemons")
+// console.log(filteredPokemons, "filteredPokemons")
   return (
     <div className="min-h-screen bg-gray-100 p-4">
       <h1 className="text-3xl font-bold text-center mb-4">Pokemon Explorer</h1>
